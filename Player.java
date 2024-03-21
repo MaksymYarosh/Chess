fe3981850869fce05166db63078977fa41612738
Maksym Yarosh <161786941+MaksymYarosh@users.noreply.github.com>
2024-03-21 16:18:53 -0400

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,19 @@
+public class Player {
+
+    public String firstName;
+    String lastName;
+    int age;
+    int rating;
+    int points;
+
+    Player() {
+    }
+
+    Player(String firstName, int age, int rating, int points) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.rating = rating;
+        this.points = points;
+    }
+}
